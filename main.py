4b6e36476f97cd42a129c2a37b8ad8ec648a4389
dorbittonn <dorbittonn@gmail.com>
2021-04-04 21:16:36 +0300

    this commit is try

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import pybullet as p
 import time
 import pybullet_data
-physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
-p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
 p.setGravity(0, 0, -10)
-planeId = p.loadURDF("plane.urdf")
-startPos = [1,1,1]
-startOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",startPos, startOrientation)
-#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-for i in range (10000):
+planeId = p.loadURDF("plane.urdf", basePosition=[0,0,0])
+tableId = p.loadURDF("table/table.urdf", basePosition=[0, 0, 0.1])
+startPos = [0, 0, 0]
+startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+itemId = p.loadURDF("random_urdfs/000/000.urdf", basePosition=[0,0,1], baseOrientation=startOrientation)
+# set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
+for i in range(10000):
     p.stepSimulation()
-    time.sleep(1./240.)
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
+    time.sleep(1. / 240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(itemId)
+print(cubePos, cubeOrn)
 p.disconnect()
