bfe1e3d480289ec90f264f852a92f829a3dc04a1
yuvalgos <yuvalgos@gmail.com>
2021-10-29 18:16:01 +0300

    used gym rescale_action wrapper for the test

--- scratch/vanilla_maze.py
+++ scratch/vanilla_maze.py
@@ -8,6 +8,7 @@ import time
 from TrainingNavigator.StepperAgent import StepperAgent
 from TrainingNavigator.NavigatorEnv import NavigatorEnv
 from stable_baselines3.common.env_checker import check_env
+from gym.wrappers.rescale_action import RescaleAction
 
 maze_map = - (cv2.imread("vanilla_map.png", cv2.IMREAD_GRAYSCALE) / 255) + 1
 maze_map = maze_map.T
@@ -24,15 +25,11 @@ device = 'auto'
 # naively try to solve it:
 agent = StepperAgent("../TrainingNavigator/StepperAgent.pt", device=device)
 
-navEnv = NavigatorEnv(maze_env=env,
+nav_env = NavigatorEnv(maze_env=env,
                       stepper_agent=agent, )
-check_env(navEnv)
-#
-# obs = navEnv.reset()
 
-# # obs = env.reset()
-# print(obs.shape)
-# for i in range(10000):
-#     action = agent.step(obs)
-#     obs, _, _, _ = navEnv.step(action)
-#     time.sleep(1/24.)
+wrapped_nav_env = RescaleAction(nav_env, -1, 1)
+check_env(wrapped_nav_env)
+
+obs = nav_env.reset()
+
