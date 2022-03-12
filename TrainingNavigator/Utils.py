d92de8d65e66c2920a43e46395ff28b6717a6324
yuvalgos <yuvalgos@gmail.com>
2022-03-12 22:20:31 +0200

    moved bottleneck map to Training Navigator dir, and the maps images to an internal directory. moved the function to compute the free-space to utils

--- TrainingNavigator/Utils.py
+++ TrainingNavigator/Utils.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("../")
+import numpy as np
 import cv2
 import MazeEnv.MazeEnv as mz
 from TrainingNavigator.StepperAgent import StepperAgent
@@ -7,12 +8,36 @@ from TrainingNavigator.NavigatorEnv import NavigatorEnv
 from stable_baselines3.common.env_checker import check_env
 from gym.wrappers.rescale_action import RescaleAction
 
+
+def get_freespace_map(maze_map, robot_cube_size):
+    # TODO move this to somewhere later
+    assert robot_cube_size % 2 == 0, "robot_cube_size must be even"
+    dist = robot_cube_size // 2
+    map_h = maze_map.shape[0]
+    map_w = maze_map.shape[1]
+
+    freespace_map = np.copy(maze_map)
+
+    for i in range(map_h - robot_cube_size):
+        for j in range(map_w - robot_cube_size):
+            window = maze_map[i: i+robot_cube_size, j: j+robot_cube_size]
+            if np.any(window > 0):
+                # center of that window is not free space
+                freespace_map[i+dist, j+dist] = 1
+
+    # # too close to the edges is not free space:
+    freespace_map[0:dist, :] = freespace_map[map_h-dist: map_h] = 1
+    freespace_map[:, 0:dist] = freespace_map[:, map_w-dist:map_w] = 1
+
+    return freespace_map
+
+
 def get_vanilla_navigator_env(start_loc=(1., 7.5), target_loc=(9, 3), show_gui=True):
     """
     create a navigator env with the vanilla maze to solve,
     NOT wrapped with RescaleAction
     """
-    map_path = "vanilla_map.png"
+    map_path = "maps/vanilla_map.png"
     maze_map = - (cv2.imread(map_path, cv2.IMREAD_GRAYSCALE) / 255) + 1
     maze_map = maze_map.T
 
---
+++ TrainingNavigator/bottleneck_maze.py
@@ -0,0 +1,37 @@
+import numpy as np
+import MazeEnv.MazeEnv as mz
+import cv2
+import time
+import matplotlib.pyplot as plt
+from Utils import get_freespace_map
+
+
+maze_map = - (cv2.imread("maps/bottleneck.png", cv2.IMREAD_GRAYSCALE) / 255) + 1
+maze_map = cv2.rotate(maze_map, cv2.cv2.ROTATE_90_CLOCKWISE)
+
+free_space = get_freespace_map(maze_map, 24)
+free_space_rotated = cv2.rotate(free_space, cv2.cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imwrite("maps/bottleneck_freespace.png", (-free_space_rotated + 1) * 255)
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.imshow(-maze_map+1, cmap='gray')
+ax1.set_title("Maze Map")
+ax2.imshow(-free_space+1, cmap='gray')
+ax2.set_title("Free Space For Robot")
+plt.show()
+
+
+env = mz.MazeEnv(maze_size=mz.MazeSize.SQUARE10,
+                 maze_map=maze_map,
+                 tile_size=0.1,
+                 start_loc=(5, 1.4),
+                 target_loc=(1.7, 8),
+                 show_gui=True)  # missing, timeout, rewards
+
+env.reset()
+
+for i in range(10**4):
+    action = [0, 1] * 4
+    env.step(action)
+    env.set_subgoal_marker((2, 2))
+    time.sleep(0.05)
