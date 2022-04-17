a8853c1761a921c07123b9331f0902c4ee9f7458
dorbittonn <dorbittonn@gmail.com>
2022-04-17 21:47:05 +0300

    hopefully dixed sb3 bug by inheriting sample_action

--- TrainingNavigator/DDPGMP.py
+++ TrainingNavigator/DDPGMP.py
@@ -4,6 +4,7 @@ import torch as th
 import numpy as np
 from NavigatorEnv import MultiStartgoalNavigatorEnv
 import math
+import gym
 
 from stable_baselines3 import DDPG
 from stable_baselines3.common.buffers import ReplayBuffer
@@ -71,6 +72,52 @@ class DDPGMP(DDPG):
         #     print(f"Debug: loaded {len(self.demonstrations)} different demonstrations")
         self.demonstrations_path = demonstrations_path
         self.demo_on_fail_prob = demo_on_fail_prob
+
+    def _sample_action(
+            self, learning_starts: int, action_noise: Optional[ActionNoise] = None
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """
+        Sample an action according to the exploration policy.
+        This is either done by sampling the probability distribution of the policy,
+        or sampling a random action (from a uniform distribution over the action space)
+        or by adding noise to the deterministic output.
+
+        :param action_noise: Action noise that will be used for exploration
+            Required for deterministic policy (e.g. TD3). This can also be used
+            in addition to the stochastic policy for SAC.
+        :param learning_starts: Number of steps before learning for the warm-up phase.
+        :return: action to take in the environment
+            and scaled action that will be stored in the replay buffer.
+            The two differs when the action space is not normalized (bounds are not [-1, 1]).
+        """
+        # Select action randomly or according to policy
+        if self.num_timesteps < learning_starts and not (self.use_sde and self.use_sde_at_warmup):
+            # Warmup phase
+            unscaled_action = np.array([self.action_space.sample()])
+        else:
+            # Note: when using continuous actions,
+            # we assume that the policy uses tanh to scale the action
+            # We use non-deterministic action in the case of SAC, for TD3, it does not matter
+            unscaled_action, _ = self.predict(self._last_obs, deterministic=False)
+            # rescale back to the original action space
+            unscaled_action = self.policy.unscale_action(unscaled_action)
+
+        # Rescale the action from [low, high] to [-1, 1]
+        if isinstance(self.action_space, gym.spaces.Box):
+            scaled_action = self.policy.scale_action(unscaled_action)
+
+            # Add noise to the action (improve exploration)
+            if action_noise is not None:
+                scaled_action = np.clip(scaled_action + action_noise(), -1, 1)
+
+            # We store the scaled action in the buffer
+            buffer_action = scaled_action
+            action = self.policy.unscale_action(scaled_action)
+        else:
+            # Discrete case, no need to normalize or clip
+            buffer_action = unscaled_action
+            action = buffer_action
+        return action, buffer_action
 
     def collect_rollouts(
             self,
@@ -231,6 +278,7 @@ class CustomActor(Actor):
         # WARNING: it must end with a tanh activation to squash the output
         # self.mu = th.nn.Sequential(...)
 
+
     def forward(self, obs: th.Tensor) -> th.Tensor:
         # assert deterministic, 'The TD3 actor only outputs deterministic actions'
         features = self.extract_features(obs)
@@ -243,12 +291,13 @@ class CustomActor(Actor):
 
         # # scale the action. all sizes (except for low, high) are torch tensors to assure differentiation
         low, high = self.action_space.low, self.action_space.high
-        # r_scaled = 2 * (r - low[0]) / (high[0] - low[0]) - 1
-        # r_scaled.clip(-1, 1)
-        # theta_scaled = 2 * (theta - low[1]) / (high[1] - low[1]) - 1
+        r_scaled = 2 * (r - low[0]) / (high[0] - low[0]) - 1
+        r_scaled = r_scaled.clip(-1, 1)
+        theta_scaled = 2 * (theta - low[1]) / (high[1] - low[1]) - 1
+        theta_scaled = theta_scaled.clip(-1, 1)
 
-        r_scaled = r.clip(low[0], high[0])
-        theta_scaled = theta.clip(low[1], high[1])
+        # r_scaled = r.clip(low[0], high[0])
+        # theta_scaled = theta.clip(low[1], high[1])
 
         return th.stack([r_scaled, theta_scaled], dim=1)
 
