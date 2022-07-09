changed settings for navigator to control robot rotation. There are bugs to fix Important: added separation between timeout truncation and terminal state!

MazeEnv/MazeEnv.py
@@ -149,7 +149,8 @@ class MazeEnv(gym.Env):
         self._pclient.configureDebugVisualizer(self._pclient.COV_ENABLE_GUI, False)  # dont show debugging windows
 
         # load maze:
-        self._maze = Maze(self._pclient, maze_size, maze_map, tile_size, self._target_loc, optimize_maze_boarders)
+        self._maze = Maze(self._pclient, maze_size, maze_map, tile_size, self._target_loc, self._target_heading,
+                          optimize_maze_boarders)
 
         # load ant robot:
         self._ant = Ant(self._pclient, self._start_loc)
@@ -180,7 +181,7 @@ class MazeEnv(gym.Env):
                                   maze_size=maze_size,
                                   video_size=self.recording_video_size,
                                   zoom=self.zoom,
-                                  fps=60)
+                                  fps=30)
 
     def step(self, action):
         if not self.is_reset:
@@ -206,7 +207,7 @@ class MazeEnv(gym.Env):
         # check status and resolve reward and is_done:
         reward = 0
         is_done = False
-        info = dict(success=False, fell=False, hit_maze=False, timeout=False)
+        info = {'success': False, 'fell': False, 'hit_maze': False, 'TimeLimit.truncated': False}
 
         if self._collision_manager.check_hit_floor():
             is_done = info['fell'] = True
@@ -235,7 +236,7 @@ class MazeEnv(gym.Env):
                     is_done = True
 
         if self.timeout_steps != 0 and self.step_count >= self.timeout_steps:
-            is_done = info['timeout'] = True
+            is_done = info['TimeLimit.truncated'] = True
             reward += self.rewards.timeout
 
         reward += self.rewards.idle
@@ -374,11 +375,7 @@ class MazeEnv(gym.Env):
         # last element is rotation angle between ant and target heading
         robot_rotation = observation[8]
         rotation_diff = self._target_heading - robot_rotation
-        rotation_sign = 1 if (0 <= rotation_diff <= 180) or (-180 >= rotation_diff >= -360) else -1
-        observation[30] = np.abs(rotation_diff) % 360
-        if observation[30] > 180:
-            observation[30] = 360 - observation[30]
-        observation[30] *= rotation_sign
+        observation[30] = self.compute_signed_rotation_diff(rotation_diff)
 
         return observation
 
@@ -406,3 +403,17 @@ class MazeEnv(gym.Env):
             print("WARNING: setting ant to position control! make sure you know what you are doing! "
                   "and please don't mix position and torque control in the same episode.")
         self._ant.set_position_control(position_control)
+
+    @staticmethod
+    def compute_signed_rotation_diff(rotation_diff):
+        """
+        given a raw angles difference, cast it to a signed angle between -pi and pi.
+        original diff may be more then pi or less than -pi.
+        """
+        unsigned_diff = np.abs(rotation_diff) % 360
+        if unsigned_diff > 180:
+            unsigned_diff = 360 - unsigned_diff
+
+        rotation_sign = 1 if (0 <= rotation_diff <= 180) or (-180 >= rotation_diff >= -360) else -1
+
+        return rotation_sign * unsigned_diff

Training/EvaluateTrainedSteppers.py
@@ -17,28 +17,29 @@ from MazeEnv.EnvAttributes import Rewards
 num_workspaces = 100
 
 
-def get_env():
-    targets_loc = np.genfromtxt("Training/workspaces/test_coords_0_6to3.csv", delimiter=',')
+def get_env(sticky_actions, timeout_steps):
+    target_list = np.genfromtxt("Training/workspaces/goals_06to3_test.csv", delimiter=',')
     tile_size = 0.1
     maze_size = mz.MazeSize.SQUARE10
     map_size = np.dot(maze_size, int(1 / tile_size))
     maze_map = make_circular_map(map_size, 5 / tile_size)
 
-    rewards = Rewards(target_distance_weight=0.01, target_distance_offset=5, fall=-1,
-                      target_arrival=1, collision=0, timeout=0, idle=0)
+    rewards = Rewards(target_distance_weight=0.01, rotation_weight=0.01, target_distance_offset=5, fall=-1,
+                      target_arrival=1, collision=0, timeout=0, idle=0,)
 
-    # TODO: add target heading list and epsilon
-    raise NotImplementedError
     maze_env = mz.MultiTargetMazeEnv(maze_size=maze_size,
                                      maze_map=maze_map,
                                      tile_size=tile_size,
                                      start_loc=(5, 5),
-                                     target_loc_list=targets_loc,
+                                     target_loc_list=target_list[:, :2],
+                                     target_heading_list=target_list[:, 2],
                                      rewards=rewards,
                                      hit_target_epsilon=0.25,
-                                     success_steps_before_done=25,
-                                     timeout_steps=400,
-                                     sticky_actions=5,
+                                     target_heading_epsilon=np.pi/9,
+                                     done_on_goal_reached=False,
+                                     timeout_steps=timeout_steps,
+                                     sticky_actions=sticky_actions,
+                                     max_goal_velocity=9999,
                                      show_gui=False,
                                      xy_in_obs=False,
                                      noisy_ant_initialization=False)
@@ -85,7 +86,7 @@ def evaluate_stepper(agent, env: mz.MultiTargetMazeEnv):
 if __name__ == "__main__":
 
     # load list of checkpoints from chosen models:
-    log_dirs = ["Training/logs/StepperV2same_params(except_max_steps)"]
+    log_dirs = ["Training/logs/StepperV2--fixed--stickyActions8_max_steps_150"]
     stepper_checkpoints = []
 
     # for log_dir in log_dirs:
@@ -96,11 +97,11 @@ if __name__ == "__main__":
     #     if int(c.split("_")[-1].split(".")[0]) < 5000000:
     #         stepper_checkpoints.remove(c)
 
-    stepper_checkpoints += glob.glob(log_dirs[0] + "/model_17600000.zip")
-    stepper_checkpoints += glob.glob(log_dirs[0] + "/model_15000000.zip")
-    stepper_checkpoints += glob.glob(log_dirs[0] + "/model_18000000.zip")
+    # stepper_checkpoints += glob.glob(log_dirs[0] + "/model_17600000.zip")
+    # stepper_checkpoints += glob.glob(log_dirs[0] + "/model_15000000.zip")
+    stepper_checkpoints += glob.glob(log_dirs[0] + "/model_25000000.zip")
 
-    env = get_env()
+    env = get_env(sticky_actions=8, timeout_steps=150)
 
     run_name_list = []
     success_once_list = []
@@ -125,7 +126,7 @@ if __name__ == "__main__":
             continue
         print(f'saving results')
 
-        run_name_list.append(model_path.split("400steps")[-1])
+        run_name_list.append(model_path.split("V2")[-1])
         success_once_list.append(success_rate_once)
         success_last_list.append(success_rate_last_step)
         fall_rate_list.append(fall_rate)

TrainingNavigator/NavEvaluation.py
@@ -91,12 +91,12 @@ class NavEvalCallback(BaseCallback):
 
         if self.eval_video_freq > 0 and \
                 self.n_calls % (self.eval_video_freq * self.eval_freq) == 0:
-            vid_path, walked_trajectory, rotation_trajectory, ws_id = self._record_video()
+            vid_path, walked_trajectory, action_trajectory, ws_id = self._record_video()
 
             log_item = {'eval_video': wandb.Video(vid_path, fps=40), 'navStep': self.n_calls}
             if self.maze_map is not None:
                 log_item['video_trajectory'] = self._get_trajectory_plot(walked_trajectory,
-                                                                         rotation_trajectory,
+                                                                         action_trajectory,
                                                                          ws_id)
 
             self.wandb_run.log(log_item)
@@ -161,16 +161,15 @@ class NavEvalCallback(BaseCallback):
         video_path = os.path.join(self.video_dir, str(self.n_calls) + '.gif')
         obs = self.eval_env.reset(create_video=True, video_path=video_path)
         walked_traj = []
-        traj_rotation = []
+        action_traj = []
         while True:
             with torch.no_grad():
                 action, _ = self.model.predict(obs, deterministic=True)
             obs, _, done, info = self.eval_env.step(action)
 
             obs_unnorm = self.eval_env.unormalize_obs_if_needed(obs)
-            walked_traj.append(obs_unnorm[:2])
-            traj_rotation.append(obs_unnorm[2])
-
+            walked_traj.append(obs_unnorm[:3])
+            action_traj.append(action)
             if done:
                 break
 
@@ -180,16 +179,23 @@ class NavEvalCallback(BaseCallback):
         if self.verbose > 0:
             print("Video recorded in %.4f secs: " % (time.time() - t_start))
 
-        return video_path, np.array(walked_traj), np.array(traj_rotation), ws_id
+        return video_path, np.array(walked_traj), np.array(action_traj), ws_id
 
-    def _get_trajectory_plot(self, walked_traj: np.ndarray, rotation_trajectory, ws_id: int) -> plt.Figure:
+    def _get_trajectory_plot(self, walked_traj: np.ndarray, action_traj, ws_id: int) -> plt.Figure:
         """
         returns a plot of the walked trajectory near the RRT planned
          trajectory for that workspace
         """
         walked_traj = walked_traj * 10
-        walked_u = np.cos(rotation_trajectory)
-        walked_v = np.sin(rotation_trajectory)
+        walked_u = np.cos(walked_traj[:, 2])
+        walked_v = np.sin(walked_traj[:, 2])
+
+        actions_u = np.cos(action_traj[:, 2])
+        actions_v = np.sin(action_traj[:, 2])
+        # in action we get r and theta, we need to convert first two elements to x and y
+        actions_y = walked_traj[:, 0] + action_traj[:, 0] * np.sin(action_traj[:, 1])
+        actions_x = walked_traj[:, 1] + action_traj[:, 0] * np.cos(action_traj[:, 1])
+
 
         with np.load(self.validation_traj_path) as demos:
             planned_traj = demos[str(ws_id)] * 10
@@ -200,15 +206,22 @@ class NavEvalCallback(BaseCallback):
         start, goal = planned_traj[0], planned_traj[-1]
 
         plt.close('all')
-        fig, axes = plt.subplots(1, 2)
-        for ax in axes:
+        fig, axes = plt.subplots(2, 2)
+        for ax in axes.flatten():
             ax.imshow(self.maze_map, cmap='gray')
             ax.plot(start[1], start[0], 'go')
             ax.plot(goal[1], goal[0], 'g+')
 
-        axes[0].set_title('Walked trajectory')
-        axes[0].quiver(walked_traj[:, 1], walked_traj[:, 0], walked_u, walked_v, color='r')
-        axes[1].set_title('RRT planned trajectory')
-        axes[1].quiver(planned_traj[:, 1], planned_traj[:, 0], planned_u, planned_v, color='r')
+        axes[0, 0].set_title('Walked Trajectory')
+        axes[0, 0].quiver(walked_traj[:, 1], walked_traj[:, 0], walked_u, walked_v, color='r')
+        axes[0, 1].set_title('RRT PlannedT Trajectory')
+        axes[0, 1].quiver(planned_traj[:, 1], planned_traj[:, 0], planned_u, planned_v, color='r')
+        axes[1, 0].set_title('Action Trajectory')
+        axes[1, 0].quiver(actions_x, actions_y, actions_u, actions_v, color='b')
+        axes[1, 1].set_title('Action + Walked Trajectory')
+        axes[1, 1].quiver(walked_traj[:, 1], walked_traj[:, 0], walked_u, walked_v, color='r')
+        axes[1, 1].quiver(actions_x, actions_y, actions_u, actions_v, color='b', alpha=0.5)
+
+        fig.tight_layout()
 
         return fig

TrainingNavigator/NavigatorEnv.py
@@ -37,6 +37,7 @@ class NavigatorEnv(gym.Env):
                  max_steps=50,
                  stepper_radius_range=(0.6, 2.5),
                  epsilon_to_hit_subgoal=0.25,
+                 epsilon_rotation_to_hit_subgoal=np.pi/9,
                  max_vel_in_subgoal=9999,
                  rewards: Rewards = Rewards(),
                  done_on_collision=False,
@@ -68,6 +69,7 @@ class NavigatorEnv(gym.Env):
         self.max_stepper_steps = max_stepper_steps
         self.max_steps = max_steps
         self.epsilon_to_hit_subgoal = epsilon_to_hit_subgoal
+        self.epsilon_rotation_to_hit_subgoal = epsilon_rotation_to_hit_subgoal
         self.max_vel_in_subgoal = max_vel_in_subgoal
         self.rewards_config = rewards
         self.done_on_collision = done_on_collision
@@ -89,26 +91,29 @@ class NavigatorEnv(gym.Env):
         if not maze_env.max_goal_velocity == max_vel_in_subgoal:
             print("WARNING: max_vel_in_subgoal is different in mazeEnv and navigatorEnv, changing mazeEnv")
             maze_env.max_goal_velocity = max_vel_in_subgoal
+        # make sure there is no requirement for angle at the rotation at goal (success is defined by the distance):
+        self.maze_env.target_heading_epsilon = np.inf
 
         self.visualize = False
         self.visualize_fps = 40
 
         if stepper_agent is None:
-            stepper_agent = StepperAgent('TrainingNavigator/StepperAgents/TorqueStepperF1500.pt', 'auto')
+            raise ValueError("please update the line below with a stepper agent (with rotation)")
+            # stepper_agent = StepperAgent('TrainingNavigator/StepperAgents/TorqueStepperF1500.pt', 'auto')
         elif isinstance(stepper_agent, str):
             stepper_agent = StepperAgent(stepper_agent, 'auto')
         self.stepper_agent = stepper_agent
 
         # Ant's current state
-        self.ant_curr_obs = np.zeros(30)
+        self.ant_curr_obs = np.zeros(31)
         self.curr_subgoal = np.zeros(2)
+        self.curr_target_rotation_at_subgoal = 0
         self.target_goal = np.array(self.maze_env._target_loc[0:2], dtype=np.float32)
-        self.ant_prev_loc = np.zeros(2)
 
-        # Action -> [radius, azimuth (in radian)]
-        self.action_space = Box(low=np.array([stepper_radius_range[0], -math.pi], dtype=np.float32),
-                                high=np.array([stepper_radius_range[1], math.pi], dtype=np.float32),
-                                shape=(2,))
+        # Action -> [radius, direction to subgoals (raidans), rotation at subgoal (radians)]
+        self.action_space = Box(low=np.array([stepper_radius_range[0], -math.pi, -math.pi], dtype=np.float32),
+                                high=np.array([stepper_radius_range[1], math.pi, math.pi], dtype=np.float32),
+                                shape=(3,))
 
 
         # Observation -> [ Agent_x, Agent_y,  Agent_Rotation, Target_x, Target_y]
@@ -121,7 +126,6 @@ class NavigatorEnv(gym.Env):
     def reset(self, **maze_env_kwargs) -> np.ndarray:
         self.curr_step = 0
         self.ant_curr_obs = self.maze_env.reset(**maze_env_kwargs)
-        self.ant_prev_loc = self.ant_curr_obs[0:2]
         self.wall_hit_count = 0
 
         nav_obs = np.concatenate([self.ant_curr_obs[0:2], [self.ant_curr_obs[8]], self.target_goal],
@@ -133,8 +137,13 @@ class NavigatorEnv(gym.Env):
             print("action:", action, "out of bounds")
 
         ant_xy = self.ant_curr_obs[0:2]
+        robot_rotation = self.ant_curr_obs[8]
+        rotation_diff = self.curr_target_rotation_at_subgoal - robot_rotation
+        rotation_diff = self.maze_env.compute_signed_rotation_diff(rotation_diff)
+
         # 2 first elements of action are range and direction to the subgoal
         self.curr_subgoal = ant_xy + pol2cart(action[0:2])
+        self.curr_target_rotation_at_subgoal = action[2]
 
         if visualize_subgoal:
             self.maze_env.set_subgoal_marker(self.curr_subgoal)
@@ -154,27 +163,33 @@ class NavigatorEnv(gym.Env):
             # to this env because it changes in each iteration in the loop
             r_theta_to_subgoal = cart2pol(self.curr_subgoal - ant_xy)
             # we used subgoal-ant_loc and not the opposite just like in mazeEnv._get_observation
+            # same applies to the rotation diff
 
             # stepper agent doesn't need x and y of the ant
             stepper_obs = self.ant_curr_obs[2:]
-            # update r and theta for the subgoal (the environment returns for the main goal)
+            # update r and theta and rotation for the subgoal (the environment returns for the main goal)
             stepper_obs[26:28] = r_theta_to_subgoal
+            stepper_obs[28] = rotation_diff
             stepper_action = self.stepper_agent.step(stepper_obs)
 
             # play ant step, reward is not required and is_done is determined using info
             self.ant_curr_obs, _, _, info = self.maze_env.step(stepper_action)
             ant_xy = self.ant_curr_obs[0:2]
             ant_velocity = np.sqrt(self.ant_curr_obs[3]**2 + self.ant_curr_obs[4]**2)
+            robot_rotation = self.ant_curr_obs[8]
+            rotation_diff = self.curr_target_rotation_at_subgoal - robot_rotation
+            rotation_diff = self.maze_env.compute_signed_rotation_diff(rotation_diff)
 
             # use info to check if finished (may override mazeEnv is_done in some cases):
-            if info['success'] or info['fell'] or info['timeout']:
+            if info['success'] or info['fell'] or info['TimeLimit.truncated']:
                 break
             if info['hit_maze']:
                 _hit_maze = True
                 if self.done_on_collision:
                     break
-            # check if close enough to subgoal and meets velocity criteria:
+            # check if meet subgoal criteria:
             if np.linalg.norm(self.curr_subgoal - ant_xy) < self.epsilon_to_hit_subgoal \
+                    and rotation_diff < self.epsilon_rotation_to_hit_subgoal \
                     and ant_velocity < self.max_vel_in_subgoal:
                 break
 
@@ -203,16 +218,14 @@ class NavigatorEnv(gym.Env):
         else:
             nav_reward += self.rewards_config.idle
 
+        nav_info['stepper_timeout'] = nav_info.pop('TimeLimit.truncated')
         self.curr_step += 1
         if self.curr_step >= self.max_steps:
-            nav_info['navigator_timeout'] = True
+            nav_info['TimeLimit.truncated'] = True
             nav_is_done = True
         else:
-            nav_info['navigator_timeout'] = False
+            nav_info['TimeLimit.truncated'] = False
 
-        nav_info['stepper_timeout'] = nav_info.pop('timeout')
-
-        self.ant_prev_loc = ant_xy
 
         nav_observation = self.normalize_obs_if_needed(nav_observation)
 
@@ -285,8 +298,6 @@ class MultiStartgoalNavigatorEnv(NavigatorEnv):
 
         self.curr_startgoal_pair_idx = start_goal_pair_idx
 
-        # TODO: changed to set_target_loc_and_heading. handle heading if needed
-        raise NotImplementedError("handle heading if needed")
         self.maze_env.set_target_loc_and_heading(self.start_goal_pairs[start_goal_pair_idx][1])
         self.maze_env.set_start_loc(self.start_goal_pairs[start_goal_pair_idx][0])
 

TrainingNavigator/TD3MP.py
@@ -334,7 +334,10 @@ class TD3MP(TD3):
         r, theta = math.sqrt(dx ** 2 + dy ** 2), math.atan2(dy, dx)
         if self.use_demo_epsilon_offset:
             r = r + self.epsilon_to_goal  # add target epsilon offset
-        return np.array([r, theta])
+
+        rotation = theta
+
+        return np.array([r, theta, rotation])
 
     def _excluded_save_params(self) -> List[str]:
         return super(TD3MP, self)._excluded_save_params() + ["demonstrations"]
@@ -378,7 +381,7 @@ class CustomActor(Actor):
         out = self.mu(features)
         # transforming to r,theta
 
-        dx, dy = out[:, 0], out[:, 1]
+        dx, dy, rotation = out[:, 0], out[:, 1], out[:, 2]
         r, theta = th.sqrt(dx ** 2 + dy ** 2), th.atan2(dy, dx)
 
         # scale the action. all sizes (except for low, high) are torch tensors to assure differentiation
@@ -388,8 +391,9 @@ class CustomActor(Actor):
         r_scaled = th.tanh(r)
         theta_scaled = 2 * (theta - low[1]) / (high[1] - low[1]) - 1
         # theta_scaled = theta_scaled.clip(-1, 1)
+        rotation_scaled = 2 * (rotation - low[2]) / (high[2] - low[2]) - 1
 
-        return th.stack([r_scaled, theta_scaled], dim=1)
+        return th.stack([r_scaled, theta_scaled, rotation_scaled], dim=1)
 
 
 class CustomTD3Policy(TD3Policy):

TrainingNavigator/TrainingMultiproc.py
@@ -51,6 +51,7 @@ if __name__ == '__main__':
     nav_env_kwargs = dict(start_goal_pairs=start_goal_pairs,
                           maze_env_kwargs=maze_env_kwargs,
                           epsilon_to_hit_subgoal=config["epsilon_to_subgoal"],
+                          epsilon_rotation_to_hit_subgoal=config["epsilon_rotation_to_subgoal"],
                           max_vel_in_subgoal=config["max_velocity_in_subgoal"],
                           rewards=config["rewards"],
                           done_on_collision=config["done_on_collision"],
@@ -84,8 +85,8 @@ if __name__ == '__main__':
     eval_nav_env.visualize_mode(False)
 
     # set up model and run:
-    exploration_noise = NormalActionNoise(mean=np.array([0] * 2),
-                                          sigma=np.array([config["exploration_noise_std"]] * 2))
+    exploration_noise = NormalActionNoise(mean=np.array([0] * 3),
+                                          sigma=np.array([config["exploration_noise_std"]] * 3))
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('running on:', device)