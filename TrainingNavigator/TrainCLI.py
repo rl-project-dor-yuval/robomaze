326fc0131fa142679f2b8a4d0dab6c7feb07fd87
yuvalgos <yuvalgos@gmail.com>
2022-08-12 15:40:41 +0300

    Fixes and update of some parameters

--- TrainingNavigator/TrainCLI.py
+++ TrainingNavigator/TrainCLI.py
@@ -21,16 +21,16 @@ if __name__ == '__main__':
     # load BN config just for keys:
     yaml_loader = yaml.Loader
     yaml_loader.add_constructor("!Rewards", Rewards.from_yaml)
-    config = yaml.load(open("TrainingNavigator/configs/BN_NoKillOnWall.yaml", "r"), yaml_loader)
+    config_temp = yaml.load(open("TrainingNavigator/configs/BN_NoKillOnWall.yaml", "r"), yaml_loader)
 
     # add all config parameters to argparse:
     parser = argparse.ArgumentParser()
-    for key in config:
-        parser.add_argument('--' + key, type=type(config[key]), default=config[key])
+    for key in config_temp:
+        parser.add_argument('--' + key, type=type(config_temp[key]),)
     # add rewards seperately:
-    reward_dict = vars(config["rewards"])
+    reward_dict = vars(config_temp["rewards"])
     for rew in reward_dict.keys():
-        parser.add_argument('--reward_' + rew, type=type(reward_dict[rew]), default=reward_dict[rew])
+        parser.add_argument('--reward_' + rew, type=type(reward_dict[rew]),)
 
     # add a mandatory arguments for map, robot and demo kind:
     parser.add_argument('--map', choices=['bottleneck', 'S-narrow', 'SpiralThick20x20', '20x20maze'],
@@ -46,12 +46,14 @@ if __name__ == '__main__':
 
     # update config with relevant args:
     for key in args.__dict__:
+        if args.__dict__[key] is None:
+            continue
         # handle rewards separately:
         if key.startswith("reward_"):
             reward_name = key.split("_")[1]
             setattr(config["rewards"], reward_name, args.__dict__[key])
         # all other parameters:
-        elif key in config.keys():
+        elif key in config.keys() and key != "rewards":
             config[key] = args.__dict__[key]
 
     # change robot and demo kind according to args:
